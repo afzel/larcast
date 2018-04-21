d18a96e23a89dc85eac36a61e676813f3eea976b
afzel <33573333+afzel@users.noreply.github.com>
2018-04-21 16:29:00 +0500

    Add files via upload

---
+++ resources/views/includes/foter.blade.php
@@ -0,0 +1,51 @@
+
+<footer>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-5 col-md-offset-1 col-sm-6">
+                    <h3>Neuron Studio</h3>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <div class="footer-copyright">
+                         <p>Copyright &copy; 2017 Your Company - Design: Tooplate</p>
+                    </div>
+               </div>
+
+               <div class="col-md-4 col-md-offset-1 col-sm-6">
+                    <h3>Talk to us</h3>
+                    <p><i class="fa fa-globe"></i> Lenina Street, Moscow</p>
+                    <p><i class="fa fa-phone"></i> 00-000-0000</p>
+                    <p><i class="fa fa-save"></i> info@company.com</p>
+               </div>
+
+               <div class="clearfix col-md-12 col-sm-12">
+                    <hr>
+               </div>
+
+               <div class="col-md-12 col-sm-12">
+                    <ul class="social-icon">
+                         <li><a href="#" class="fa fa-facebook"></a></li>
+                         <li><a href="#" class="fa fa-twitter"></a></li>
+                         <li><a href="#" class="fa fa-google-plus"></a></li>
+                         <li><a href="#" class="fa fa-dribbble"></a></li>
+                         <li><a href="#" class="fa fa-linkedin"></a></li>
+                    </ul>
+               </div>
+               
+          </div>
+     </div>
+</footer>
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- SCRIPTS -->
+
+<script src="{{asset('/js/jquery.js')}}"></script>
+<script src="{{asset('/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('/js/particles.min.js')}}"></script>
+<script src="{{asset('/js/app.js')}}"></script>
+<script src="{{asset('/js/jquery.parallax.js')}}"></script>
+<script src="{{asset('/js/smoothscroll.js')}}"></script>
+<script src="{{asset('/js/custom.js')}}"></script>
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,174 @@
+
+ @include('/includes/header')
+<body>
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-wordpress">
+          <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- Navigation section  -->
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+     <div class="container">
+
+          <div class="navbar-header">
+               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+               </button>
+               <a href="index" class="navbar-brand">Neuron</a>
+          </div>
+          <div class="collapse navbar-collapse">
+               <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="/index">Главная </a></li>
+                    <li><a href="/about">О нас</a></li>
+                    <li><a href="/gellary">Галерия</a></li>
+                    <li><a href="/contact">Контакты</a></li>
+               </ul>
+          </div>
+
+  </div>
+</div>
+
+<!-- Home Section -->
+
+<section id="home" class="main-home parallax-section">
+     <div class="overlay"></div>
+     <div id="particles-js"></div>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-12 col-sm-12">
+                    <h1>Привет. Это Неурон!</h1>
+                    <h4>Отзывчивый блог HTML CSS Website Template</h4>
+                    <a href="#blog" class="smoothScroll btn btn-default">Открой для себя больше</a>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Blog Section -->
+
+<section id="blog">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-1 col-md-10 col-sm-12">
+                    <div class="blog-post-thumb">
+                         <div class="blog-post-image">
+                              <a href="single-post.html">
+                                   <img src="{{asset('/images/blog-image1.jpg')}}" class="img-responsive" alt="Blog Image">
+                              </a>
+                         </div>
+                         <div class="blog-post-title">
+                              <h3><a href="single-post.html">Мы помогаем вам создавать идеальный современный дизайн</a></h3>
+                         </div>
+                         <div class="blog-post-format">
+                              <span><a href="#"><img src="images/author-image1.jpg" class="img-responsive img-circle"> Jen Lopez</a></span>
+                              <span><i class="fa fa-date"></i> Январ 01, 2018</span>
+                              <span><a href="#"><i class="fa fa-comment-o"></i> 35 Комментарии</a></span>
+                         </div>
+                         <div class="blog-post-des">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                              <a href="single-post.html" class="btn btn-default">Продолжить чтение
+</a>
+                         </div>
+                    </div>
+
+                    <div class="blog-post-thumb">
+                         <div class="blog-post-image">
+                              <a href="single-post.html">
+                                   <img src="images/blog-image2.jpg" class="img-responsive" alt="Blog Image">
+                              </a>
+                         </div>
+                         <div class="blog-post-title">
+                              <h3><a href="single-post.html">In pretium tellus et ante accumsan</a></h3>
+                         </div>
+                         <div class="blog-post-format">
+                              <span><a href="#"><img src="images/author-image2.jpg" class="img-responsive img-circle"> Leo Dennis</a></span>
+                              <span><i class="fa fa-date"></i> June 10, 2017</span>
+                              <span><a href="#"><i class="fa fa-comment-o"></i> 48 Comments</a></span>
+                         </div>
+                         <div class="blog-post-des">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                              <a href="single-post.html" class="btn btn-default">Продолжить чтение</a>
+                         </div>
+                    </div>
+
+                    <div class="blog-post-thumb">
+                         <div class="blog-post-video">
+                              <div class="embed-responsive embed-responsive-16by9">
+                                   <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Pgmx7z49OEk" allowfullscreen></iframe>
+                              </div>
+                         </div>
+                         <div class="blog-post-title">
+                              <h3><a href="single-post.html">Nam interdum maximus dolor faucibus</a></h3>
+                         </div>
+                         <div class="blog-post-format">
+                              <span><a href="#"><img src="images/author-image1.jpg" class="img-responsive img-circle"> Jen Lopez</a></span>
+                              <span><i class="fa fa-date"></i> May 30, 2017</span>
+                              <span><a href="#"><i class="fa fa-comment-o"></i> 63 Comments</a></span>
+                         </div>
+                         <div class="blog-post-des">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                              <a href="single-post.html" class="btn btn-default">Continue Reading</a>
+                         </div>
+                    </div>
+
+                    <div class="blog-post-thumb">
+                         <div class="blog-post-image">
+                              <a href="single-post.html">
+                                   <img src="images/blog-image3.jpg" class="img-responsive" alt="Blog Image">
+                              </a>
+                         </div>
+                         <div class="blog-post-title">
+                              <h3><a href="single-post.html">The ingredients that make a great burger</a></h3>
+                         </div>
+                         <div class="blog-post-format">
+                              <span><a href="#"><img src="images/author-image2.jpg" class="img-responsive img-circle"> Leo Dennis</a></span>
+                              <span><i class="fa fa-date"></i> Апрел 18, 2017</span>
+                              <span><a href="#"><i class="fa fa-comment-o"></i> 124 Комментарии</a></span>
+                         </div>
+                         <div class="blog-post-des">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                              <a href="single-post.html" class="btn btn-default">Продолжить чтение</a>
+                         </div>
+                    </div>
+
+                    <div class="blog-post-thumb">
+                         <div class="blog-post-image">
+                              <a href="single-post.html">
+                                   <img src="images/blog-image4.jpg" class="img-responsive" alt="Blog Image">
+                              </a>
+                         </div>
+                         <div class="blog-post-title">
+                              <h3><a href="single-post.html">Vestibulum vel mauris nec ex tempus</a></h3>
+                         </div>
+                         <div class="blog-post-format">
+                              <span><a href="#"><img src="images/author-image1.jpg" class="img-responsive img-circle"> Jen Lopez</a></span>
+                              <span><i class="fa fa-date"></i> Март 12, 2017</span>
+                              <span><a href="#"><i class="fa fa-comment-o"></i> 256 Комментарии</a></span>
+                         </div>
+                         <div class="blog-post-des">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                              <a href="single-post.html" class="btn btn-default">Продолжить чтение</a>
+                         </div>
+                    </div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Footer Section -->
+
+ @include('/includes/foter')
+
+</body>
+</html>
